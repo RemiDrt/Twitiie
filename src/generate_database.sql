1de891468e11bbac9999a93dcdaf42745f4ce20d
Guillaume Descroix <guillaumedescroix@outlook.fr>
2021-01-28 12:27:10 +0100

    script generate modif BD

--- src/generate_database.sql
+++ src/generate_database.sql
@@ -1,23 +1,35 @@
-CREATE TABLE PATTERN(
-  id_pattern NUMBER(10) PRIMARY,
-  pattern VARCHA2(50) NOT NULL
+CREATE SEQUENCE PATTERN_ID
+  start 1
+  increment 1;
+
+CREATE SEQUENCE SCORE_ID
+  start 1
+  increment 1;
+
+CREATE SEQUENCE PLAYER_ID
+  start 1
+  increment 1;
+
+CREATE TABLE PATTERN (
+  id_pattern    INTEGER DEFAULT nextval('PATTERN_ID') PRIMARY KEY,
+  pattern       VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE SCORE(
-  id_score NUMBER(10) PRIMARY KEY,
-  score NUMBER(4) NOT NULL,
-  id_pattern NUMBER(4) NOT NULL,
+CREATE TABLE SCORE (
+  id_score      INTEGER DEFAULT nextval('SCORE_ID') PRIMARY KEY,
+  score         INTEGER NOT NULL,
+  id_pattern    INTEGER NOT NULL,
   CONSTRAINT cirIdChoice FOREIGN KEY(id_pattern) REFERENCES PATTERN
 );
 
-CREATE TABLE PLAYER(
-  id_player NUMBER(10) PRIMARY KEY,
-  pseudo VARCHAR2(25) NOT NULL,
-  mail VARCHAR2(40) NOT NULL,
-  password VARCHAR2(40) NOT NULL,
-  id_score_tot NUMBER(10),
-  id_score_mon NUMBER(10),
-  id_score_week NUMBER(10),
+CREATE TABLE PLAYER (
+  id_player     INTEGER DEFAULT nextval('PLAYER_ID') PRIMARY KEY,
+  pseudo        VARCHAR(25) NOT NULL,
+  mail          VARCHAR(40) NOT NULL,
+  password      VARCHAR(40) NOT NULL,
+  id_score_tot  INTEGER,
+  id_score_mon  INTEGER,
+  id_score_week INTEGER,
   CONSTRAINT cirScoreTot FOREIGN KEY(id_score_tot) REFERENCES SCORE(id_score),
   CONSTRAINT cirScoreMon FOREIGN KEY(id_score_mon) REFERENCES SCORE(id_score),
   CONSTRAINT cirScoreWeek FOREIGN KEY(id_score_week) REFERENCES SCORE(id_score)
