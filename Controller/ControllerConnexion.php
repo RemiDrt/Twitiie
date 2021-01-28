62b9ac130586044a8d133f167da32c25f55e7099
Nicolas <nicolas.forget77@gmail.com>
2021-01-28 14:37:53 +0100

    14h37 Nico

--- Controller/ControllerConnexion.php
+++ Controller/ControllerConnexion.php
@@ -1,6 +1,24 @@
+<?php 
+
+class Controller_connexion extends Controller{
+
+  public function action_connexion(){
+    $mod = Model::getModel(); 
+    $req = $mod->getUserConnexion();
+    $data = ['User' => $req];
+    $this->render("connexion", $data);
+  }
 
 
+	/**
+	* Action par défaut du contrôleur (à définir dans les classes filles)
+	*/
+	public function action_default(){
+		$this->action_home();
+	}
 
+
+}
 
 
 
