0d2f73849e6d521abe38fa84f90a08f37c43da64
RemiDrt <rd.duret@hotmail.fr>
2021-01-29 11:40:58 +0100

    html viewInfosJoueur

--- Views/ViewInfosJoueur.php
+++ Views/ViewInfosJoueur.php
@@ -1,8 +1,29 @@
 <?php require "Header.php"; ?>
 <?php 
 var_dump($data); 
-echo "les infos d'un joueurs"
 ?>
+<table>
+<tr><th><?=$data["Pseudo"]?></th></tr>
+<tr>
+    <th>Score</th>
+    <th>Pattern</th>
+</tr>
+<tr>
+    <th>Ever</th>
+    <td><?=$data["PlayerScoreTot"]?></td>
+    <td><?=$data["PlayerPatternTot"]?></td>
+</tr>
+<tr>
+    <th>Month</th>
+    <td><?=$data["PlayerScoreMon"]?></td>
+    <td><?=$data["PlayerPatternMon"]?></td>
+</tr>
+<tr>
+    <th>Week</th>
+    <td><?=$data["PlayerScoreWeek"]?></td>
+    <td><?=$data["PlayerPatternWeek"]?></td>
+</tr>
+</table>
 
 
 
